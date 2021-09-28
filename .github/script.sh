24be9ee49f6314d967002499a0458ae1ebb023ef
David Cejtlin <david.cejtlin@ecd.rs>
2021-09-28 23:51:26 +0200

    test

--- .github/script.sh
+++ .github/script.sh
@@ -1,4 +1,5 @@
 echo $SSH_KEY >> key
+cat key
 sed -i "s/latest-tagged-version/$GITHUB_RUN_NUMBER/g" docker-compose.yaml
 scp -i key docker-compose.yaml ubuntu@18.192.4.4:/home/ubuntu
 ssh -i key ubuntu@18.192.4.4 'cd /home/ubuntu/ && docker-compose up -d'
