0328cd87603ad5e628bca4cbe4953566fc44083e
David Cejtlin <david.cejtlin@ecd.rs>
2021-09-28 23:55:02 +0200

    disable host key checking

--- .github/script.sh
+++ .github/script.sh
@@ -1,5 +1,5 @@
 echo $SSH_KEY >> key
-cat key
+SSH_OPTIONS=-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null
 sed -i "s/latest-tagged-version/$GITHUB_RUN_NUMBER/g" docker-compose.yaml
-scp -i key docker-compose.yaml ubuntu@18.192.4.4:/home/ubuntu
-ssh -i key ubuntu@18.192.4.4 'cd /home/ubuntu/ && docker-compose up -d'
+scp -i key $SSH_OPTIONS docker-compose.yaml ubuntu@18.192.4.4:/home/ubuntu
+ssh -i key $SSH_OPTIONS ubuntu@18.192.4.4 'cd /home/ubuntu/ && docker-compose up -d'
