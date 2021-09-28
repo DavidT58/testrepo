40549ad7c5bae5c71212d32f2040c5f9388a5408
David Cejtlin <david.cejtlin@ecd.rs>
2021-09-28 23:58:11 +0200

    c

--- .github/script.sh
+++ .github/script.sh
@@ -1,5 +1,6 @@
 echo $SSH_KEY >> key
 SSH_OPTIONS=-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null
+ssh-keyscan -H 18.192.4.4 > ~/.ssh/known_hosts
 sed -i "s/latest-tagged-version/$GITHUB_RUN_NUMBER/g" docker-compose.yaml
 scp -i key $SSH_OPTIONS docker-compose.yaml ubuntu@18.192.4.4:/home/ubuntu
 ssh -i key $SSH_OPTIONS ubuntu@18.192.4.4 'cd /home/ubuntu/ && docker-compose up -d'
