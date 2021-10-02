a05675663348333719e538ef13ddc168074d16e5
David Cejtlin <david.cejtlin@ecd.rs>
2021-10-02 20:40:09 +0200

    test remote docker host

--- .github/script.sh
+++ .github/script.sh
@@ -3,8 +3,6 @@ chmod 400 key
 SSH_OPTIONS='-o StrictHostKeyChecking=no'
 # mkdir -p ~/.ssh
 # ssh-keyscan -H 18.192.4.4 > ~/.ssh/known_hosts
-cat $TEST
 sed -i "s/latest-tagged-version/$GITHUB_RUN_NUMBER/g" docker-compose.yaml
-cat docker-compose.yaml
 scp -i key $SSH_OPTIONS docker-compose.yaml ubuntu@18.192.4.4:/home/ubuntu
 ssh -i key $SSH_OPTIONS ubuntu@18.192.4.4 'cd /home/ubuntu/ && docker-compose up -d'
